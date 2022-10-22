b4023fb54328d60efa34bd95732fc4c8cf1a958b
trhgquan <quanthcstpbt@gmail.com>
2022-10-22 19:36:45 +0700

    Added crf POS-Tagging

--- practice/CONLL-POSTagging/download.sh
+++ practice/CONLL-POSTagging/download.sh
@@ -1,2 +1,4 @@
 #!bin/bash
-curl -LJ "https://github.com/Hironsan/anago/raw/master/data/conll2003/en/ner/train.txt" -o "train.label"
+curl -LJ "https://github.com/Hironsan/anago/raw/master/data/conll2003/en/ner/train.txt" -o "train.txt"
+curl -LJ "https://github.com/Hironsan/anago/raw/master/data/conll2003/en/ner/valid.txt" -o "valid.txt"
+curl -LJ "https://github.com/Hironsan/anago/raw/master/data/conll2003/en/ner/test.txt" -o "test.txt"
